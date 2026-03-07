f69a1fa50460501a609108d6f7f4a8f076b2b349
Sayanraj-99 <162550828+Sayanraj-99@users.noreply.github.com>
2026-03-08 03:28:04 +0530

    Update database.py

--- database.py
+++ database.py
@@ -26,7 +26,7 @@ def init_db():
     """)
     
     # Content queue table
-  One more thing (very important)
+ # One more thing (very important)
 Your database must support status.
 Make sure your queue table has:
 status TEXT DEFAULT 'pending'
