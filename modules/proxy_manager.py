3a621f2dc872c3e5ff1e6cacd62a1f73412d79da
Sayanraj-99 <162550828+Sayanraj-99@users.noreply.github.com>
2026-03-07 22:06:08 +0530

    Update proxy_manager.py

--- modules/proxy_manager.py
+++ modules/proxy_manager.py
@@ -1,1 +1,29 @@
+import database
 
+
+def add_proxy(proxy):
+
+    conn = database.get_connection()
+    c = conn.cursor()
+
+    c.execute(
+        "INSERT INTO proxies(proxy) VALUES(?)",
+        (proxy,)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def get_proxies():
+
+    conn = database.get_connection()
+    c = conn.cursor()
+
+    c.execute("SELECT proxy FROM proxies")
+
+    rows = c.fetchall()
+
+    conn.close()
+
+    return [r[0] for r in rows]
