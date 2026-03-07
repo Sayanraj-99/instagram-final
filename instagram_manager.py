9ce0557fb70a86420cc62e7e40e0248b57a64360
Sayanraj-99 <162550828+Sayanraj-99@users.noreply.github.com>
2026-03-07 21:45:17 +0530

    Update instagram_manager.py

--- instagram_manager.py
+++ instagram_manager.py
@@ -1,24 +1,54 @@
 import database
 from instagrapi import Client
+from instagram_auth import InstagramAuth
 
 class InstagramManager:
+    def __init__(self):
+        self.auth = InstagramAuth()
+    
     def add_account(self, username, password):
+        """Add Instagram account"""
         conn = database.get_connection()
         c = conn.cursor()
-        c.execute("INSERT INTO accounts(username, password) VALUES(?,?)", 
-                 (username, password))
-        conn.commit()
-        conn.close()
+        try:
+            c.execute("INSERT INTO accounts(username, password) VALUES(?,?)", 
+                     (username, password))
+            conn.commit()
+            return True
+        except sqlite3.IntegrityError:
+            return False
+        finally:
+            conn.close()
     
     def delete_post(self, username, password, post_id):
-        client = Client()
-        client.login(username, password)
-        client.media_delete(post_id)
+        """Delete Instagram post"""
+        client = self.auth.login_account(username, password)
+        if client:
+            client.media_delete(post_id)
+            return True
+        return False
     
-    def get_stats(self, account_id):
+    def get_stats(self, username):
+        """Get account statistics"""
         conn = database.get_connection()
         c = conn.cursor()
-        c.execute("SELECT SUM(views) FROM analytics WHERE account_id=?", (account_id,))
-        total_views = c.fetchone()[0] or 0
+        c.execute("""
+            SELECT 
+                posts_count, 
+                total_views,
+                AVG(total_views * 1.0 / posts_count) as avg_views,
+                MAX(views) as max_views
+            FROM accounts a
+            LEFT JOIN analytics an ON a.id = an.account_id
+            WHERE username = ?
+        """, (username,))
+        
+        stats = c.fetchone()
         conn.close()
-        return {'total_views': total_views}
+        
+        return {
+            'total_posts': stats[0] or 0,
+            'total_views': stats[1] or 0,
+            'avg_views': round(stats[2] or 0, 2),
+            'max_views': stats[3] or 0
+        }
