a55a9473459c3b3d7a057ee87f14f0476954bcb5
Sayanraj-99 <162550828+Sayanraj-99@users.noreply.github.com>
2026-03-07 22:05:29 +0530

    Update account_manager.py

--- modules/account_manager.py
+++ modules/account_manager.py
@@ -1,1 +1,42 @@
+import database
 
+def add_account(username):
+
+    conn = database.get_connection()
+    c = conn.cursor()
+
+    c.execute(
+        "INSERT INTO accounts(username) VALUES(?)",
+        (username,)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def delete_account(username):
+
+    conn = database.get_connection()
+    c = conn.cursor()
+
+    c.execute(
+        "DELETE FROM accounts WHERE username=?",
+        (username,)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def get_accounts():
+
+    conn = database.get_connection()
+    c = conn.cursor()
+
+    c.execute("SELECT username FROM accounts")
+
+    rows = c.fetchall()
+
+    conn.close()
+
+    return [r[0] for r in rows]
